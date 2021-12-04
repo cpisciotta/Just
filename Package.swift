9d731be2e05f433a6ca3db10f87ea1525c634e0b
Charles Pisciotta <cpisciottadeveloping@gmail.com>
2021-12-04 18:18:35 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,14 @@
 // swift-tools-version:5.5
 import PackageDescription
 
-let package = Package(name: "Just")
+let package = Package(
+    name: "Just",
+    platforms: [
+       .macOS(.v12)
+    ],
+    targets: [
+        .target(
+            name: "Just"
+        )
+    ]
+)
